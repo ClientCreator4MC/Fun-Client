b94160d11384bc399a24a18c4d9fb58f125ed5f9
ClientCreator4MC <creatingclientsformc@gmail.com>
2025-07-22 12:53:50 -0400

    Update NoFall.java

--- src/main/java/me/alpha432/oyvey/features/modules/player/NoFall.java
+++ src/main/java/me/alpha432/oyvey/features/modules/player/NoFall.java
@@ -1,21 +1,23 @@
-package me.alpha432.oyvey.features.modules.player;
+package me.alpha432.oyvey.features.modules.movement;
 
-import me.alpha432.oyvey.OyVey;
 import me.alpha432.oyvey.features.modules.Module;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 
 public class NoFall extends Module {
+    private final MinecraftClient mc = MinecraftClient.getInstance();
+
     public NoFall() {
-        super("NoFall", "Removes fall damage", Category.PLAYER, true, false, false);
+        super("NoFall", Category.MOVEMENT, "Prevents fall damage.");
     }
 
     @Override
-    public void onUpdate() {
-        if (!mc.player.isOnGround() && OyVey.positionManager.getFallDistance() > 3) {
-            boolean bl = mc.player.horizontalCollision;
-            PlayerMoveC2SPacket.Full pakcet = new PlayerMoveC2SPacket.Full(mc.player.getX(), mc.player.getY() + 0.000000001, mc.player.getZ(),
-                    mc.player.getYaw(), mc.player.getPitch(), false, bl);
-            mc.player.networkHandler.sendPacket(pakcet);
+    public void onTick() {
+        if (mc.player == null || mc.getNetworkHandler() == null) return;
+
+        if (mc.player.fallDistance > 2.5f) {
+            // Send safe on-ground spoof packet
+            mc.getNetworkHandler().sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
         }
     }
 }
